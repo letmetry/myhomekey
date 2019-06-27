54161b2601d0560ffa7a15fc0cdbfe2e89704740
letmetry <momi.rt@gmail.com>
2019-06-28 09:35:39 +1000

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,244 @@ app.post('/papp', (req, res)=> {//portable device app request
 });
 
 app.get('/', function (req, res) {
-    res.send(`<b>My</b> first express http server`);
+    res.send(`
+
+<style>
+body { 
+  background-color: #DDDDDD; 
+  font: 30px sans-serif; 
+  height: 350px;
+  background: url(http://media.giphy.com/media/Jrd9E2kuPuOYM/giphy.gif) 50%;
+}
+
+#maintext {
+  color: #A7DD3C;
+  text-shadow: 2px 0 0px #800040, 
+    3px 2px 0px rgba(77,0,38,0.5),
+    3px 0 3px #FF002B, 5px 0 3px #800015, 
+    6px 2px 3px rgba(77,0,13,0.5), 
+    6px 0 9px #FF5500, 
+    8px 0 9px #802A00, 
+    9px 2px 9px rgba(77,25,0,0.5), 
+    9px 0 18px #FFD500, 
+    11px 0 18px #806A00, 
+    12px 2px 18px rgba(77,66,0,0.5), 
+    12px 0 30px #D4FF00, 
+    14px 0 30px #6A8000, 
+    15px 2px 30px rgba(64,77,0,0.5), 
+    15px 0 45px #80FF00, 
+    17px 0 45px #408000, 
+    17px 2px 45px rgba(38,77,0,0.5);
+  color: #A7DD3C;
+  text-align: center;
+  font-family: Impact, Charcoal, sans-serif;
+  font-size: 70px;
+  letter-spacing: 5.2px;
+  word-spacing: 5.2px;
+  font-weight: 800;
+  text-decoration: none;
+  font-style: normal;
+  font-variant: normal;
+  text-transform: none;
+}
+
+.ball-wrapper {
+    position: fixed;
+    width: 120px;
+    height: 300px;
+    margin-left: -60px;
+    left: 50%;
+    top: 20%;
+}
+
+.ball {
+    position: absolute;
+    width: 120px;
+    height: 120px;
+    border-radius: 70px;
+    background: -webkit-linear-gradient(top,  rgba(187,187,187,1) 0%,rgba(119,119,119,1) 99%);
+    background: -moz-linear-gradient(top,  rgba(187,187,187,1) 0%,rgba(119,119,119,1) 99%);
+    background: linear-gradient(top,  rgba(187,187,187,1) 0%,rgba(119,119,119,1) 99%);
+    box-shadow: inset 0 -5px 15px rgba(255,255,255,0.4), inset -2px -1px 40px rgba(0,0,0,0.4), 0 0 1px #000;
+    -webkit-animation: jump 1s infinite;
+    -moz-animation: jump 1s infinite;
+    animation: jump 1s infinite;
+}
+
+.ball::after {
+    content: "";
+    position: absolute;
+    width: 60px;
+    height: 30px;
+    border-radius: 40px / 20px;
+    left: 30px;
+    top: 10px;
+    background: -webkit-linear-gradient(top,  rgba(232,232,232,1) 0%,rgba(232,232,232,1) 1%,rgba(255,255,255,0) 100%);
+    background: -moz-linear-gradient(top,  rgba(232,232,232,1) 0%,rgba(232,232,232,1) 1%,rgba(255,255,255,0) 100%);
+    background: linear-gradient(top,  rgba(232,232,232,1) 0%,rgba(232,232,232,1) 1%,rgba(255,255,255,0) 100%);
+}
+
+.ball-shadow {
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    width: 50px;
+    height: 65px;
+    border-radius: 30px / 40px;
+    margin-left: -25px;
+    background: rgba(20, 20, 20, 0.1);
+    box-shadow: 0px 0 20px 35px rgba(20,20,20,.1);
+    -webkit-transform: scaleY(0.3);
+    -moz-transform: scaleY(0.3);
+    transform: scaleY(0.3);
+    -webkit-animation: shrink 1s infinite;
+    -moz-animation: shrink 1s infinite;
+    animation: shrink 1s infinite;
+}
+
+/**
+ *	animation
+ */
+
+@-webkit-keyframes jump {
+    0% {
+	top: 0;
+	-webkit-animation-timing-function: ease-in;
+    }
+    40% {}
+    50% {
+	top: 140px;
+	height: 120px;
+	-webkit-animtion-timing-function: ease-out;
+    }
+    55% {
+	top: 160px;
+	height: 100px;
+	border-radius: 70px/60px;
+	-webkit-animation-timing-function: ease-in;
+    }
+    65% {
+	top: 110px;
+	height: 120px;
+	border-radius: 50%;
+	-webkit-animation-timing-function: ease-out;
+    }
+    95% {
+	top: 0;
+	-webkit-animation-timing-function: ease-in;
+    }
+    100% {
+	top: 0;
+	-webkit-animation-timing-function: ease-in;
+    }
+}
+
+@-moz-keyframes jump {
+    0% {
+	top: 0;
+	-moz-animation-timing-function: ease-in;
+    }
+    40% {}
+    50% {
+	top: 140px;
+	height: 120px;
+	-moz-animtion-timing-function: ease-out;
+    }
+    55% {
+	top: 160px;
+	height: 100px;
+	border-radius: 70px/60px;
+	-moz-animation-timing-function: ease-in;
+    }
+    65% {
+	top: 110px;
+	height: 120px;
+	border-radius: 50%;
+	-moz-animation-timing-function: ease-out;
+    }
+    95% {
+	top: 0;
+	-moz-animation-timing-function: ease-in;
+    }
+    100% {
+	top: 0;
+	-moz-animation-timing-function: ease-in;
+    }
+}
+
+
+@-webkit-keyframes shrink {
+    0% {
+	bottom: 0;
+	margin-left: -30px;
+	width: 60px;
+	height: 75px;
+	background: rgba(20, 20, 20, .1);
+	box-shadow: 0px 0 20px 35px rgba(20,20,20,.1);
+	border-radius: 30px / 40px;
+	-webkit-animation-timing-function: ease-in;
+    }
+    50% {
+	bottom: 30px;
+	margin-left: -10px;
+	width: 20px;
+	height: 5px;
+	background: rgba(20, 20, 20, .3);
+	box-shadow: 0px 0 20px 35px rgba(20,20,20,.3);
+	border-radius: 20px / 20px;
+	-webkit-animation-timing-function: ease-out;
+    }
+    100% {
+	bottom: 0;
+	margin-left: -30px;
+	width: 60px;
+	height: 75px;
+	background: rgba(20, 20, 20, .1);
+	box-shadow: 0px 0 20px 35px rgba(20,20,20,.1);
+	border-radius: 30px / 40px;
+	-webkit-animation-timing-function: ease-in;
+    }
+}
+@-moz-keyframes shrink {
+    0% {
+	bottom: 0;
+	margin-left: -30px;
+	width: 60px;
+	height: 75px;
+	background: rgba(20, 20, 20, .1);
+	box-shadow: 0px 0 20px 35px rgba(20,20,20,.1);
+	border-radius: 30px / 40px;
+	-moz-animation-timing-function: ease-in;
+    }
+    50% {
+	bottom: 30px;
+	margin-left: -10px;
+	width: 20px;
+	height: 5px;
+	background: rgba(20, 20, 20, .3);
+	box-shadow: 0px 0 20px 35px rgba(20,20,20,.3);
+	border-radius: 20px / 20px;
+	-moz-animation-timing-function: ease-out;
+    }
+    100% {
+	bottom: 0;
+	margin-left: -30px;
+	width: 60px;
+	height: 75px;
+	background: rgba(20, 20, 20, .1);
+	box-shadow: 0px 0 20px 35px rgba(20,20,20,.1);
+	border-radius: 30px / 40px;
+	-moz-animation-timing-function: ease-in;
+    }
+    }
+
+     </style>
+<div id="maintext">MOMIZ CLOUD</div>
+<div class="ball-wrapper">
+    <div class="ball"></div>
+    <div class="ball-shadow"></div>
+</div>
+`);
 });
 
 
