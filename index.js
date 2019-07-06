2fb8b05a01fa6c40edddd89e8902fdee4a4e2f16
letmetry <momi.rt@gmail.com>
2019-07-06 23:32:07 +1000

    Update index.js

--- index.js
+++ index.js
@@ -343,6 +343,7 @@ io.on('connection',(ws)=>{
    	ws.on('close',()=>{console.log('HWS socket is disconnected');});
  	ws.on('message',(message)=>{
    		console.log(message.trim());
+		console.log(JSON.parse(message.trim())["deviceid"]);
 	});
 });
 
