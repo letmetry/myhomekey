67a05d8e2bf6656e073e410f6022a2935cc559f2
letmetry <momi.rt@gmail.com>
2019-12-03 21:23:32 +1100

    Update index.js

--- index.js
+++ index.js
@@ -372,7 +372,8 @@ return res.send(`["as","ed","bd"]`);
 app.post('/ifttt', (req, res)=> {//portable device app request
     let iftttJson = JSON.stringify(req.body);
 	iftttJson = JSON.parse(iftttJson);
-	console.log("IFTTT: ",iftttJson[0]);
+	console.log("Val 1: ",iftttJson[0]);
+	console.log("Val 2: ",iftttJson[1]);
 	/*
 		try {
     			let iftttJson = JSON.parse(req.body);
